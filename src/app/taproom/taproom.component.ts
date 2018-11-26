914cf9f3e069e6c47803e2d0929090973dc85851
Sean Peterson <Seanpeterson11@gmail.com>
2018-11-26 16:24:27 -0600

    update styling on mobile and add buttons for merch and contract brew

--- src/app/taproom/taproom.component.ts
+++ src/app/taproom/taproom.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { OnTap } from './../models/onTap.model';
 import { OnTapService } from './../onTap.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-taproom',
@@ -11,10 +12,19 @@ import { OnTapService } from './../onTap.service';
 export class TaproomComponent implements OnInit {
 
   onTap;
-  constructor(private OnTapService: OnTapService) { }
+  constructor(private OnTapService: OnTapService, private router: Router) {
+    router.events.subscribe(s => {
+          if (s instanceof NavigationEnd) {
+            const tree = router.parseUrl(router.url);
+            if (tree.fragment) {
+              const element = document.querySelector("#" + tree.fragment);
+              if (element) { element.scrollIntoView(); }
+            }
+          }
+        });
+  }
 
   ngOnInit() {
-    window.scrollTo(0, 0);
     this.onTap = this.OnTapService.getOnTap();
   }
 
