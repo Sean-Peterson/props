3224d750d24d8386255753f16dfa08bcf3d15ee3
Sean Peterson <Seanpeterson11@gmail.com>
2018-11-26 17:09:33 -0600

    add scroll to bottom for contract brew

--- src/app/taproom/taproom.component.ts
+++ src/app/taproom/taproom.component.ts
@@ -12,20 +12,46 @@ import { Router, NavigationEnd } from '@angular/router';
 export class TaproomComponent implements OnInit {
 
   onTap;
+  frag: string;
+  public counter: number = 0;
   constructor(private OnTapService: OnTapService, private router: Router) {
     router.events.subscribe(s => {
           if (s instanceof NavigationEnd) {
             const tree = router.parseUrl(router.url);
             if (tree.fragment) {
               const element = document.querySelector("#" + tree.fragment);
-              if (element) { element.scrollIntoView(); }
+              if (element) {
+                this.frag = 'bottom';
+                this.increment();
+              }
             }
           }
         });
   }
+  goDown(){
+    if(this.frag == "bottom"){
+      window.scroll({
+        top:500000,
+        left:0,
+        behavior: 'smooth'
+      });
+      console.log('WTF');
+    }
+  }
+
+  increment(){
+    setTimeout(() =>
+    {
+      this.goDown();
+    },
+    500);
+    }
+
 
   ngOnInit() {
     this.onTap = this.OnTapService.getOnTap();
   }
+
+
 
 }
